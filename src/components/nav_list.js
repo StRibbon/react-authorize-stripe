2aae9f7e9ce1c5c38d63ccb4536b0776328a4935
Stephen Ribbon <stribbon@icloud.com>
2016-08-11 20:32:23 -0700

    active working now

--- src/components/nav_list.js
+++ src/components/nav_list.js
@@ -1,16 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
 import NavListItem from './nav_list_item';
 // import classNames from 'classnames';
-const NavList = (props) => {
-  const navItems = props.list.map((item) =>
-    <NavListItem key={item.label} item={item} />
-  );
-  return (
-    <div className="nav-bar">
-      {navItems}
-    </div>
-  );
-};
+class NavList extends Component {
+  constructor() {
+    super();
+    this.state = { activeItem: null };
+  }
+  render() {
+    const handleClick = (item) => {
+      this.setState({ activeItem: item.label });
+    };
+    const navItems = this.props.list.map((item) =>
+      <NavListItem
+        handleClick={() => handleClick(item)}
+        isActive={this.state.activeItem === item.label}
+        key={item.label}
+        item={item}
+      />
+    );
+
+    return (
+      <div className="nav-bar">
+        {navItems}
+      </div>
+    );
+  }
+}
 
 NavList.propTypes = { list: React.PropTypes.array };
 
--- src/components/nav_list_item.js
+++ src/components/nav_list_item.js
@@ -9,20 +9,14 @@ class NavListItem extends Component {
       className: props.item.className,
       active: false
     };
-    this.handleClick = this.handleClick.bind(this);
-  }
-
-  handleClick() {
-    const active = !this.state.isActive;
-    this.setState({ isActive: active });
   }
 
   render() {
     const classes = classNames({ 'nav-item': true });
-    const isActive = classNames({ active: this.state.isActive });
+    const isActive = classNames({ active: this.props.isActive });
     return (
       <div className={`${classes} ${this.state.className}`}>
-        <a className={isActive} onClick={this.handleClick}>{this.state.label}</a>
+        <a className={isActive} onClick={this.props.handleClick}>{this.state.label}</a>
       </div>
     );
   }
@@ -30,7 +24,9 @@ class NavListItem extends Component {
 NavListItem.propTypes = {
   label: React.PropTypes.string,
   className: React.PropTypes.string,
-  item: React.PropTypes.object
+  item: React.PropTypes.object,
+  handleClick: React.PropTypes.func,
+  isActive: React.PropTypes.bool
 };
 
 export default NavListItem;
